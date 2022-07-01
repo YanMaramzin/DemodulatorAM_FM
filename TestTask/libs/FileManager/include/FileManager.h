386d772e243cdba9b64bad878852d29422d68091
Yan <yan.maramzin@mail.ru>
2022-07-02 02:55:25 +0300

    Заголовочный файл для файл менеджера

--- TestTask/libs/FileManager/include/FileManager.h
+++ TestTask/libs/FileManager/include/FileManager.h
@@ -5,11 +5,17 @@
 #include <string>
 #include <vector>
 
-namespace fileManager {
+class fileManager {
+    public:
+    fileManager(const std::string &fileName);
+    ~fileManager();
 
-   std::vector<Complex<float>> ReadFile(const std::string &fileName);
-   void SaveSignal(const std::vector<Complex<float>>& savedSignal, const std::string& fileName);
-}
+    std::vector<Complex<float>> ReadFile();
+    void SaveSignal(const std::vector<Complex<float>>& savedSignal, const std::string& fileName);
+
+    private:
+    std::string fileName;
+};
 
 
 #endif // FILEMANAGER_H
