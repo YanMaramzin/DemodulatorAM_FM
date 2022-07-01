3bb47334b03ca97b726efab1be3c4c14e1055ae9
Yan <yan.maramzin@mail.ru>
2022-07-01 13:13:07 +0300

    Добавление возможности считывать файлы, в которых хранятся данные типа float

--- TestTask/libs/FileManager/source/FileManager.cpp
+++ TestTask/libs/FileManager/source/FileManager.cpp
@@ -3,16 +3,27 @@
 std::vector<Complex<float>> fileManager::ReadFile(const std::string &fileName)
 {
     std::ifstream file(fileName,std::ios::binary);
+    std::vector<float> buff;
     std::vector<Complex<float>> sig;
+
     if(!file.is_open())
     {
         std::cout<<"Error"<<std::endl;
     }
     else
     {
-        Complex<float> tmp;
+        float tmp;
         for(size_t i=0;file.read((char*)&tmp,sizeof(tmp));i++)
-            sig.push_back(tmp);
+        {
+            buff.push_back(tmp);
+        }
+
+        for(size_t i=0;i<buff.size()-1;i++)
+        {
+            Complex<float> b(buff[i],buff[i+1]);
+            sig.push_back(b);
+        }
+
 
     }
     file.close();
