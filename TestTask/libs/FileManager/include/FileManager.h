6265fd834314ba2a106edde1ec7c82720ab13dd5
YanMaramzin <yan.maramzin@mail.ru>
2022-07-10 16:29:50 +0300

    Изменение вклассе файл-менеджера: можно считывать файлы, в которых данные разных типов, исправлено ошибка, при которой из файла считывались нули, которых там не должно было быть

--- TestTask/libs/FileManager/include/FileManager.h
+++ TestTask/libs/FileManager/include/FileManager.h
@@ -6,17 +6,46 @@
 #include <vector>
 
 
-class fileManager {
-    public:
-    fileManager(const std::string &fileName);
-    ~fileManager();
 
-    std::vector<Complex<float>> ReadFile();
-    void SaveSignal(const std::vector<float>& savedSignal, const std::string& fileName);
+class FileManager {
+    public:
+    template<typename T>
+    std::vector<Complex<float>> ReadFile(const std::string &fileName)
+    {
+        std::ifstream file(fileName,std::ios::binary);
+        std::vector<Complex<float>> sig;
+
+        if(!file.is_open())
+        {
+            std::cout<<"Error"<<std::endl;
+        }
+        else
+        {
+
+            file.seekg (0, file.end);
+
+            int length = file.tellg();
+
+            sig.reserve(length/(2*sizeof (T)));
+            file.seekg (0, file.beg);
+            T * buffer = new T [length];
+            // read data as a block:
+            file.read ((char *)buffer,length);
+            for(size_t i=0;i<length/sizeof(T);i+=2)
+             {
+                Complex<float> tmp(buffer[i],buffer[i+1]);
+                sig.push_back(tmp);
+             }
+            file.close();
+
+            delete[] buffer;
+        }
+        return sig;
+    }
+    void SaveSignal(const std::vector<Complex<float>>& savedSignal, const std::string& fileName);
     void SaveWAV(const std::vector<float>& savedSignal, const std::string& fileName, const int sampleRate,const int bitDepht);
 
-    private:
-    std::string fileName;
+
 };
 
 
--- TestTask/libs/FileManager/source/FileManager.cpp
+++ TestTask/libs/FileManager/source/FileManager.cpp
@@ -1,52 +1,20 @@
 #include "FileManager.h"
 
 
-fileManager::fileManager(const std::string &fileName):fileName(fileName)
-{}
-fileManager::~fileManager()
-{}
-std::vector<Complex<float>> fileManager::ReadFile()
-{
-    std::ifstream file(fileName,std::ios::binary);
-    std::vector<Complex<float>> sig;
 
-    if(!file.is_open())
-    {
-        std::cout<<"Error"<<std::endl;
-    }
-    else
-    {
-          file.seekg(0,file.end);
-          int lenght=file.tellg();
-          float * buff=new float [lenght];
-
-          file.seekg(0,file.beg);
-          file.read((char *)buff,lenght);
-          for(size_t i=0;i<lenght-2;i+=2)
-          {
-              Complex<float> tmp(buff[i],buff[i+1]);
-              sig.push_back(tmp);
-          }
-
-          file.close();
-          delete [] buff;
-    }
-
-
-    return sig;
-}
-
-void fileManager::SaveSignal(const std::vector<float>& savedSignal, const std::string& fileName)
+void FileManager::SaveSignal(const std::vector<Complex<float>>& savedSignal, const std::string& fileName)
 {
     std::ofstream file(fileName,std::ios::binary);
-    file.write((char*)&savedSignal,savedSignal.size());
+
+    for(size_t i=0;i<savedSignal.size();i++)
+        file.write((char*)&savedSignal[i],sizeof(Complex<float>));
     file.close();
 }
 
 void writeToFile(std::ofstream &file,int value,int size){
     file.write(reinterpret_cast<char*>(&value),size);
 }
-void fileManager::SaveWAV(const std::vector<float>& savedSignal, const std::string& fileName,const int sampleRate,const int bitDepth)
+void FileManager::SaveWAV(const std::vector<float>& savedSignal, const std::string& fileName,const int sampleRate,const int bitDepth)
 {
     std::ofstream file(fileName,std::ios::binary);
     //Header chunk
