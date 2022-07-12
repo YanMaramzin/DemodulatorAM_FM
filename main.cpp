6d9c5681449c84f2cbc1b08f6baf532fdb5f6831
YanMaramzin <yan.maramzin@mail.ru>
2022-07-12 10:47:56 +0300

    Изменение вmain демонстрация работы демодулятора и файл менеджера, работа фильтра скользящего среднего

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
-#include <FileManager.h>
+#include "FileManager.h"
+#include "Filter.h"
+#include "Downsample.h"
+#include "Demodulator.h"
+#include <cmath>
+#include <algorithm>
+
 
 
 int main()
 {
-    std::vector<Complex<float>> sig=fileManager::ReadFile("/home/yan/file1EuropaPlus.bin");
-    for(size_t i=0;i<sig.size();i++)
-        std::cout<<sig[i];
-    return 0;
+       FileManager fileManager;
+       std::vector<Complex<float>> signalAM=fileManager.ReadFile<int32_t>("../DemodulatorAmFm/AM.dat");
+       std::vector<Complex<float>> signalFM=fileManager.ReadFile<float>("../DemodulatorAmFm/FM.bin");
+
+
+       int bitDepth=16;
+       //Работа АМ-демодулятора
+       int fsAM=16000;
+       DemodulatorAM AM;
+       std::vector<float> demodAM=AM.Demodulate(signalAM);
+       fileManager.SaveWAV(demodAM,"DemodAM.wav",fsAM,bitDepth);
+
+
+       //Работа ЧМ-демодулятора
+       int fsFM=500000;
+       int frequencyReduction=5;
+       DemodulatorFM FM;
+       std::vector<float> demodFM=FM.Demodulate(signalFM);
+       demodFM=downsample(demodFM,frequencyReduction);
+       fileManager.SaveWAV(demodFM,"DemodFM.wav",fsFM/frequencyReduction,bitDepth);
+
+       int windowSize=5;
+       //Нерекурсивный фильтр
+        FilterMovingAverageNonRec FiltNonRec;
+        std::vector<float> filtSig=FiltNonRec.filtration(demodFM,windowSize);
+       //Рекурсивный фильтр
+        FilterMovingAverageRec FiltRec;
+        std::vector<float> filtSig1=FiltRec.filtration(demodFM,windowSize);
+
+        for(size_t i=0; i<50;i++)
+            std::cout<<demodFM[i]<<" "<<filtSig[i]<<" "<<filtSig1[i]<<std::endl;
+
+       return 0;
 }
